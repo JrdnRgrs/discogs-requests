cc91304d73fd475c4bce74ad42c34427d32f78af
Jordan Rogers <jrdnrgrs@gmail.com>
2024-05-16 14:43:40 -0500

    use TSDiscogsTool library for api

--- backend/src/routes/collection.js
+++ backend/src/routes/collection.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const axios = require('axios');
-const rateLimit = require('./rateLimit');  // Ensure you import the middleware
-
+const rateLimit = require('../rateLimit');  // Ensure you import the middleware
+const Dotenv = require('dotenv');
+const Client = require('js_ts_discogs_api_v2_library').default;
+Dotenv.config();
 const router = express.Router();
 
 router.get('/collection', async (req, res) => {
@@ -11,18 +13,26 @@ router.get('/collection', async (req, res) => {
 
     try {
         const url = `https://api.discogs.com/users/jrdnrgrs/collection/folders/0/releases?page=${page}&per_page=50&sort=${sort}&sort_order=${sortOrder}`;
-        // You can use 3 methods
+        // You can use 4 methods
 
-        // Token (signed as user)
+        // 1. Token (signed as user)
         // const response = await axios.get(url, {
         //     headers: { 'Authorization': `Discogs token=${process.env.DISCOGS_API_KEY}` }
         // });
-        // App Auth (signed in as app)
-        const response = await axios.get(url, {
-            headers: { 'Authorization': `Discogs key=${process.env.DISCOGS_API_CON_KEY}, secret=${process.env.DISCOGS_API_CON_SECRET}` }
-        });
-        // No Auth (public, no pictures)
+        // 2. App Auth (signed in as app)
+        // const response = await axios.get(url, {
+        //     headers: { 'Authorization': `Discogs key=${process.env.DISCOGS_API_CON_KEY}, secret=${process.env.DISCOGS_API_CON_SECRET}` }
+        // });
+        // 3. No Auth (public, no pictures)
         //const response = await axios.get(url);
+
+        // 4. npm library js_ts_discogs_api_v2_library
+        const client = new Client({
+            token: process.env.DISCOGS_API_TOKEN,
+            discogsUserName: process.env.DISCOGS_USER_NAME,
+        });
+
+        const response = await client.getUserFolderContents('0', page, sort, sortOrder);
 
         // Discogs rate limits, lets try not to hit it
         // https://www.discogs.com/developers/#page:home,header:home-rate-limiting
@@ -36,8 +46,6 @@ router.get('/collection', async (req, res) => {
         res.status(500).send('Error retrieving collection from Discogs');
     }
 }, rateLimit);  // Apply the middleware after the API call
-
-
 
 const requests = [];  // This will store our requests in memory
 
